cc2b6656b64225fabb4d825c9bc7577a538746a0
sgeorgieva <santiya.georgieva@gmail.com>
2024-05-21 13:45:58 +0300

    add missing Button import

--- web/frontend/components/ProductsCard.jsx
+++ web/frontend/components/ProductsCard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Card, TextContainer, Text } from "@shopify/polaris";
+import { Card, TextContainer, Text, Button } from "@shopify/polaris";
 import { Toast } from "@shopify/app-bridge-react";
 import { useTranslation } from "react-i18next";
 import { useAppQuery, useAuthenticatedFetch } from "../hooks";
