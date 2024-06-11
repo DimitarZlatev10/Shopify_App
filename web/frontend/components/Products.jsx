f05d60d2094e5e69b28f47bb6e666ffd92bfebef
Dimitar Zlatev <dimitarzlatev02@gmail.com>
2024-06-11 15:07:24 +0300

    added some components for writing and reading products metafields

--- web/frontend/components/Products.jsx
+++ web/frontend/components/Products.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import {Page, LegacyCard, DataTable, Link, Button} from '@shopify/polaris';
-import {PlusIcon} from '@shopify/polaris-icons';
-import { useAppQuery, useAuthenticatedFetch } from '../hooks';
-import { DEFAULT_PRODUCTS_COUNT } from '../../constants';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Page, LegacyCard, DataTable, Link, Button } from "@shopify/polaris";
+import { PlusIcon } from "@shopify/polaris-icons";
+import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+import { DEFAULT_PRODUCTS_COUNT } from "../../constants";
 // import './styles.css';
 // import styles from './Products.module.css';
 // import "@shopify/polaris/build/esm/styles.css" in index.js
 import "../styles.css";
-
 
 const Products = () => {
   const emptyToastProps = { content: null };
@@ -34,14 +33,14 @@ const Products = () => {
 
   const handleGenerateTOC = async (event, element) => {
     setIsLoading(true);
-    const response = await fetch("/api/generateTocPerProduct", { 
+    const response = await fetch("/api/generateTocPerProduct", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         gid: element.id,
-        descriptionHtml: element.descriptionHtml
+        descriptionHtml: element.descriptionHtml,
       }),
     });
 
@@ -59,48 +58,53 @@ const Products = () => {
         error: true,
       });
     }
-  }
+  };
 
-  const rows = data && data.length > 0 && data.map((element) => {
-    return [<Link
-        removeUnderline
-        url={`https://admin.shopify.com/store/dimitar-shop-app-test/products/${element.id}`}
-        key="emerald-silk-gown"
-      >
-        {element.title}
-      </Link>,
-      element.isTocGenerated ? 'Yes' : 'No',
-      !element.isTocGenerated ? <Button icon={PlusIcon} onClick={(event) => handleGenerateTOC(event, element)}>Generate TOC</Button> : null]
-  });
+  const rows =
+    data &&
+    data.length > 0 &&
+    data.map((element) => {
+      return [
+        <Link
+          removeUnderline
+          url={`https://admin.shopify.com/store/dimitar-shop-app-test/products/${element.id}`}
+          key="emerald-silk-gown"
+        >
+          {element.title}
+        </Link>,
+        element.isTocGenerated ? "Yes" : "No",
+        !element.isTocGenerated ? (
+          <Button
+            icon={PlusIcon}
+            onClick={(event) => handleGenerateTOC(event, element)}
+          >
+            Generate TOC
+          </Button>
+        ) : null,
+      ];
+    });
 
-  return ( 
-    data && data.length > 0 && 
-    <div className="products-wrapper">
-      <Page 
-        title="Products" 
-        fullWidth
-        pagination={{
-          hasPrevious: true,
-          hasNext: true,
-        }}
-        // style={{backgroundColor: 'red'}}
-        // className="p-0"
-        // className={styles.test}
-        // className="test"
-        // className="!p-0"
+  return (
+    data &&
+    data.length > 0 && (
+      <div className="products-wrapper">
+        <Page
+          title="Products"
+          fullWidth
+          pagination={{
+            hasPrevious: true,
+            hasNext: true,
+          }}
+          // style={{backgroundColor: 'red'}}
+          // className="p-0"
+          // className={styles.test}
+          // className="test"
+          // className="!p-0"
         >
           <LegacyCard>
             <DataTable
-              columnContentTypes={[
-                'text',
-                'text',
-                'numeric',
-              ]}
-              headings={[
-                'Name',
-                'TOC',
-                'Actions'
-              ]}
+              columnContentTypes={["text", "text", "numeric"]}
+              headings={["Name", "TOC", "Actions"]}
               rows={rows}
               pagination={{
                 hasNext: true,
@@ -110,7 +114,8 @@ const Products = () => {
           </LegacyCard>
         </Page>
       </div>
-    );
-}
+    )
+  );
+};
 
 export default Products;
